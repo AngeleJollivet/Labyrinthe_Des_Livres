b305054879dae5ec46f63d1a7d99974d3126af56
Mirasoa <mirasoaj@gmail.com>
2024-03-26 09:09:30 +0100

    ajout

---
+++ Labyrinthe/src/test/java/Labyrinthe_des_Livres/efrei/Labyrinthe_des_Livres/LabyrintheDesLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package Labyrinthe_des_Livres.efrei.Labyrinthe_des_Livres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabyrintheDesLivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
