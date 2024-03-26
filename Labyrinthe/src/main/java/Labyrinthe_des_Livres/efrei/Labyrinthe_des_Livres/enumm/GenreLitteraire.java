41454e83aa0dca962a6e76beaa2a426cd79ef7f3
Mirasoa <mirasoaj@gmail.com>
2024-03-26 12:17:19 +0100

    enum

--- Labyrinthe/src/main/java/Labyrinthe_des_Livres/efrei/Labyrinthe_des_Livres/enumm/GenreLitteraire.java
+++ Labyrinthe/src/main/java/Labyrinthe_des_Livres/efrei/Labyrinthe_des_Livres/enumm/GenreLitteraire.java
@@ -1,4 +1,5 @@
 package Labyrinthe_des_Livres.efrei.Labyrinthe_des_Livres.enumm;
 
 public enum GenreLitteraire {
+    FANTAISIE, BIOGRAPHIE, HISTOIRE, ROMANCE, PHILOSOPHIE, POLARD
 }
--- Labyrinthe/src/main/java/Labyrinthe_des_Livres/efrei/Labyrinthe_des_Livres/enumm/PublicCible.java
+++ Labyrinthe/src/main/java/Labyrinthe_des_Livres/efrei/Labyrinthe_des_Livres/enumm/PublicCible.java
@@ -1,4 +1,5 @@
 package Labyrinthe_des_Livres.efrei.Labyrinthe_des_Livres.enumm;
 
 public enum PublicCible {
+    ENFANT, JEUNE, ETUDIANT, ADULT
 }
